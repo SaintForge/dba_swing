756f772658b91d503fce90c7a08f07b6a35373e9
SaintForge <mr.spartan-117@yandex.ru>
2019-11-30 16:27:52 +0300

    Added buttons

--- src/dba/Main.java
+++ src/dba/Main.java
@@ -24,6 +24,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.*;
+import javax.swing.plaf.TabbedPaneUI;
 import javax.swing.JFrame; 
 import javax.swing.JPanel;
 import javax.swing.border.Border;
@@ -56,35 +57,35 @@ class SettingsData
 	String mtmPort;
 	String profileUser;
 	String profilePassword;
-
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public String getSqlServer() {
 		return sqlServer;
 	}
 	public void setSqlServer(String sqlServer) {
 		this.sqlServer = sqlServer;
 	}
-
+	
 	public String getMtmPort() {
 		return this.mtmPort;
 	}
-	public void setMtmPort(String mtpPort) {
+	public void setMtmPort(String mtmPort) {
 		this.mtmPort = mtmPort;
 	}
-
+	
 	public String getProfieUser() {
 		return profileUser;
 	}
 	public void setProfileUser(String profileUser) {
 		this.profileUser = profileUser;
 	}
-
+	
 	public String getProfilePassword() {
 		return profilePassword;
 	}
@@ -95,26 +96,28 @@ class SettingsData
 
 class SettingsDialog extends JDialog
 {
-	JButton okButton = new JButton("Create"); 
+	private static final long serialVersionUID = 1L;
+	
+	JButton okButton = new JButton("Create");
 	JButton cancelButton = new JButton("Cancel"); 
 	SettingsData settings = new SettingsData();
-
+	
 	JPanel panel;
 	SpringLayout springLayout;
 	Component prevComponent;
-
+	
 	public void addElement(String labelName) 
 	{
 		JLabel label = new JLabel();
 		label.setText(labelName);
-
+		
 		this.panel.add(label);
 		this.springLayout.putConstraint("West", label, 10, "West", this.panel);
 		this.springLayout.putConstraint("North", label, 30, "North", this.prevComponent);
-
+		
 		Component comp = new JTextField();
 		comp.setName(labelName.substring(0, labelName.length()));
-
+		
 		this.panel.add(comp);
 		this.springLayout.putConstraint("West", comp, 120, "West", this.panel);
 		this.springLayout.putConstraint("East", comp, 0, "East", this.cancelButton);
@@ -131,55 +134,55 @@ class SettingsDialog extends JDialog
 		this.springLayout = new SpringLayout();
 		this.panel = new JPanel(this.springLayout);
 		this.prevComponent = this.panel;
-
+		
 		int w = 300;
     	int h = 250;
     	setPreferredSize(new Dimension(w, h));
-
+		
 		addElement("Name:");
 		addElement("SQL Server:");
 		addElement("MTM Port:");
 		addElement("Profile User:");
 		addElement("Profile Password:");
-
+		
 		this.okButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				SettingsDialog.this.saveSettings();
-
-				String name = settings.getName();
-				if (name != null && !name.isEmpty()) {
-					System.out.println("not null");
-					System.out.println(settings.getName());
-					SettingsDialog.this.setVisible(false);
-				}
-			}
-		});
-
+										{
+										public void actionPerformed(ActionEvent arg0)
+										{
+										SettingsDialog.this.saveSettings();
+										
+										String name = settings.getName();
+										if (name != null && !name.isEmpty()) {
+										System.out.println("not null");
+										System.out.println(settings.getName());
+										SettingsDialog.this.setVisible(false);
+										}
+										}
+										});
+		
 		this.panel.add(this.okButton);
 		this.springLayout.putConstraint("South", this.okButton, -10, "South", this.panel);
 		this.springLayout.putConstraint("East", this.okButton, -10, "West", this.cancelButton);
-
+		
 		this.cancelButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				SettingsDialog.this.setVisible(false);
-			}
-		});
-
+											{
+											public void actionPerformed(ActionEvent arg0)
+											{
+											SettingsDialog.this.setVisible(false);
+											}
+											});
+		
 		this.panel.add(this.cancelButton);
 		this.springLayout.putConstraint("South", this.cancelButton, -10, "South", this.panel);
 		this.springLayout.putConstraint("East", this.cancelButton, -10, "East", this.panel);
-
+		
 		Container pane = getContentPane();
 		pane.add(panel);
-
+		
 		Rectangle rect = owner.getBounds();
 		int x = rect.x + (rect.width - w) / 2;
 		int y = rect.y + (rect.height - h) / 2;
-
+		
 		setLocation(x, y);
 		setResizable(false);
 	}
@@ -190,10 +193,10 @@ class SettingsDialog extends JDialog
 		
 		dialog.pack();
 		dialog.setVisible(true);
-
+		
 		return dialog.settings;
 	}
-
+	
 	public void saveSettings()
 	{
 		this.settings.setName(getProperty("Name:"));
@@ -202,7 +205,7 @@ class SettingsDialog extends JDialog
 		this.settings.setProfileUser(getProperty("Profile User:"));
 		this.settings.setProfilePassword(getProperty("Profile Password:"));
 	}
-
+	
 	public String getProperty(String name)
 	{
 		for (int i = 0; i < this.panel.getComponents().length; i++)
@@ -227,60 +230,62 @@ class DBForm
 {
 	EnvironmentData data;
 	JPanel panel = new JPanel(new GridBagLayout());
-
+	
 	DBForm(SettingsData settings)
 	{
 		JPanel tableList = createNewTable("Table Name", "Description");
 		JPanel tableInfo = createNewTable("Properties", "Value");
 		JPanel fieldList = createNewTable("Field Name", "Description");
 		JPanel fieldInfo = createNewTable("Properties", "Value");
-
+		
 		JTextField tableSearch = new JTextField();
-		this.panel.add(tableSearch,  new GridBagConstraints(0, 0, 100, 1, 1.0, 0.01, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 100, 0));
-
+		this.panel.add(tableSearch,  new GridBagConstraints(0, 0, 1, 1, 1.0, 0.01, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 100, 0));
+		this.panel.add(new JButton("Refresh"),  new GridBagConstraints(2, 0, 1, 1, 1.0, 0.01, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(2, 2, 2, 80), 0, 0));
+		this.panel.add(new JButton("Settings"),  new GridBagConstraints(2, 0, 1, 1, 1.0, 0.01, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 0, 0));
+		
 		JTextField fieldSearch = new JTextField();
-		this.panel.add(fieldSearch,  new GridBagConstraints(1, 0, 100, 1, 1.0, 0.01, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 100, 0));
-
-		this.panel.add(tableList,  new GridBagConstraints(0, 1, 1, 1, 1.0, 0.7, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
-
-		this.panel.add(tableInfo,  new GridBagConstraints(0, 2, 1, 1, 1.0, 0.35, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
-
-        this.panel.add(fieldList,  new GridBagConstraints(1, 1, 1, 1, 1.0, 0.7, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
-
-        this.panel.add(fieldInfo,  new GridBagConstraints(1, 2, 1, 1, 1.0, 0.35, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
-
+		this.panel.add(fieldSearch,  new GridBagConstraints(3, 0, 1, 1, 1.0, 0.01, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 100, 0));
+		this.panel.add(new JLabel(""),  new GridBagConstraints(5, 0, 1, 1, 1.0, 0.01, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(2, 2, 2, 140), 0, 0));
+		this.panel.add(new JButton("Refresh"),  new GridBagConstraints(5, 0, 1, 1, 1.0, 0.01, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(2, 2, 2, 2), 0, 0));
+		
+		this.panel.add(tableList,  new GridBagConstraints(0, 1, 3, 1, 1.0, 0.7, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
+		this.panel.add(tableInfo,  new GridBagConstraints(0, 2, 3, 1, 1.0, 0.35, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
+		this.panel.add(fieldList,  new GridBagConstraints(3, 1, 3, 1, 1.0, 0.7, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
+		this.panel.add(fieldInfo,  new GridBagConstraints(3, 2, 3, 1, 1.0, 0.35, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
+		
 		this.panel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 	}
-
+	
 	public JPanel createNewTable(String firstName, String secondName)
 	{
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setBorder(BorderFactory.createEtchedBorder());
-
+		panel.setBorder(BorderFactory.createEtchedBorder());
+		
 		DefaultTableModel tableModel = new DefaultTableModel();
 		tableModel.addColumn(firstName);
 		tableModel.addColumn(secondName);
-
+		
 		JTable table = new JTable(tableModel);
 		table.setPreferredScrollableViewportSize(table.getPreferredSize());
 		table.setFillsViewportHeight(true);
 		table.setFont(new Font("Courier New", Font.PLAIN, 14));
 		table.setShowVerticalLines(true);
 		JScrollPane scrollTable = new JScrollPane(table);
-
+		
 		panel.add(scrollTable, BorderLayout.CENTER);
 		
 		return panel;
 	}
-
+	
 	public JPanel getPanel() {
 		return panel;
 	}
-
+	
 	public void setPanel(JPanel panel) {
 		this.panel = panel;
 	}
-
+	
 	public static DBForm createNewForm(SettingsData settings)
 	{
 		DBForm dbForm = new DBForm(settings);
@@ -288,16 +293,29 @@ class DBForm
 	}
 }
 
-class MainFrame extends JFrame
+class PopUpDemo extends JPopupMenu {
+    JMenuItem anItem;
+    public PopUpDemo() {
+        anItem = new JMenuItem("Click Me!");
+        add(anItem);
+    }
+}
+
+class MainFrame extends JFrame 
 {
+	
+	private static final long serialVersionUID = 1L;
+	
+	int active_tab = 0;
 	int tab_amount = 0;
+	JTabbedPane tabs;
 	
 	MainFrame()
 	{
 		this.setSize(1200, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JTabbedPane tabs = new JTabbedPane(JTabbedPane.TOP);
+		tabs = new JTabbedPane(JTabbedPane.TOP);
 		
 		tabs.addTab("vtbdevnew", null, DBForm.createNewForm(new SettingsData()).getPanel(), null);
 		tabs.setMnemonicAt(0, KeyEvent.VK_1);
@@ -307,32 +325,45 @@ class MainFrame extends JFrame
 		
 		this.tab_amount = 3;
 		tabs.addTab(" + ", null, new JPanel(), null);
+		
 		tabs.addChangeListener(new ChangeListener()
-		{
-			public void stateChanged(ChangeEvent evt)
-			{
-				JTabbedPane tabbedPane = (JTabbedPane)evt.getSource();
-
-				// Create a new Tab
-				if (tabbedPane.getSelectedIndex() == (tab_amount - 1))
-				{
-					System.out.println("new tab");
-					SettingsData settings = openSettingsDialog();
-					
-					if (settings.name == null || settings.name.isEmpty()) {
-						tabbedPane.setSelectedIndex(tabs.getTabCount() - 2);
-						return;
-					}
-					
-					tabbedPane.remove(tabbedPane.getTabCount() - 1);
-					tab_amount++ ;
-					
-					tabbedPane.addTab(settings.name, null, DBForm.createNewForm(settings).getPanel(), "new env");
-					tabbedPane.addTab(" + ", null, new JPanel(), null);
-					tabbedPane.setSelectedIndex(tabs.getTabCount() - 2);
-				}
-			}
-		});
+							   {
+							   public void stateChanged(ChangeEvent evt)
+							   {
+							   System.out.println("mouse input");
+							   JTabbedPane tabbedPane = (JTabbedPane)evt.getSource();
+							   
+							   int prevIndex = active_tab;
+							   int tabIndex = tabbedPane.getSelectedIndex();
+							   System.out.println(tabIndex);
+							   System.out.println(active_tab);
+							   // Create a new Tab
+							   if (tabIndex == (tab_amount - 1))
+							   {
+							   System.out.println("new tab");
+							   SettingsData settings = openSettingsDialog();
+							   
+							   if (settings.name == null || settings.name.isEmpty()) {
+							   tabbedPane.setSelectedIndex(active_tab);
+							   System.out.println("no shit");
+							   return;
+							   }
+							   
+							   tabbedPane.remove(tabbedPane.getTabCount() - 1);
+							   tab_amount++ ;
+							   
+							   tabbedPane.addTab(settings.name, null, DBForm.createNewForm(settings).getPanel(), "new env");
+							   tabbedPane.addTab(" + ", null, new JPanel(), null);
+							   tabbedPane.setSelectedIndex(tabs.getTabCount() - 2);
+							   System.out.println("hello");
+							   }
+							   else 
+							   {
+							   System.out.println("hi");
+							   active_tab = tabIndex;
+							   }
+							   }
+							   });
 		
 		tabs.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
 		
@@ -352,6 +383,36 @@ class MainFrame extends JFrame
 		SettingsData result = SettingsDialog.createDialog(this);
 		return (result);
 	}
+	
+	// @Override
+	// public void mouseClicked(MouseEvent e)
+	// {
+	// 	PopUpDemo menu = new PopUpDemo();
+    //     menu.show(e.getComponent(), e.getX(), e.getY());
+	
+	// 	System.out.println("hello");
+	
+	// 	JTabbedPane tabbedPane = (JTabbedPane)e.getSource();
+	
+	// 	if (tabbedPane.getSelectedIndex() == (tab_amount - 1))
+	// 	{
+	// 		System.out.println("new tab");
+	// 		SettingsData settings = openSettingsDialog();
+	
+	// 		if (settings.name == null || settings.name.isEmpty()) {
+	// 			tabbedPane.setSelectedIndex(tabs.getTabCount() - 2);
+	// 			return;
+	// 		}
+	
+	// 		tabbedPane.remove(tabbedPane.getTabCount() - 1);
+	// 		tab_amount++ ;
+	
+	// 		tabbedPane.addTab(settings.name, null, DBForm.createNewForm(settings).getPanel(), "new env");
+	// 		tabbedPane.addTab(" + ", null, new JPanel(), null);
+	// 		tabbedPane.setSelectedIndex(tabs.getTabCount() - 2);
+	// 	}
+	
+	// }
 }
 
 public class Main
